c8ff5ca89bf73b8c5ac049709d4486adbc8e50b2
xianfengting <1142631421@qq.com>
2019-04-04 20:13:51 +0800

    Include the Android Studio project files

---
+++ app/src/main/java/com/src_resources/anti996/browser/android/AppMainActivity.kt
@@ -0,0 +1,12 @@
+package com.src_resources.anti996.browser.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AppMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_main)
+    }
+}
