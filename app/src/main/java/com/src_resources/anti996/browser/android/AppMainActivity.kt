65be77a4b6bac1a28a7c6d8a6cf8743bb94ecfee
xianfengting <1142631421@qq.com>
2019-04-04 21:41:44 +0800

    Edit AppMainActivity.kt

--- app/src/main/java/com/src_resources/anti996/browser/android/AppMainActivity.kt
+++ app/src/main/java/com/src_resources/anti996/browser/android/AppMainActivity.kt
@@ -2,11 +2,17 @@ package com.src_resources.anti996.browser.android
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class AppMainActivity : AppCompatActivity() {
+
+    private lateinit var mainWebView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_main)
+
+        mainWebView = findViewById(R.id.mainWebView)
+        mainWebView.loadUrl("https://996.icu")
     }
 }
